73837c6c18dc5ebfda2f004bd13b2a090cf9d107
Colleen McCormack <colleen.mccormack@yale.edu>
2016-02-23 14:06:14 -0500

    Update databases differently.

--- social_todo/settings.py
+++ social_todo/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 if os.environ.get('PRODUCTION'):
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+    DATABASES['default'] = db_from_env
     print(DATABASES)
 
 # Password validation
